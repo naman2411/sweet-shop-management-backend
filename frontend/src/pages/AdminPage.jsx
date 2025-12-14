7bfcf63380f03353d13f2c9cafaaf92b42a23404
naman2411 <namangoyal0007@gmail.com>
2025-12-14 15:49:03 +0530

    Added React Frontend

---
+++ frontend/src/pages/AdminPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AdminPage() {
+    return (
+        <div className="container mt-5">
+            <h2 className="text-danger">Admin Dashboard</h2>
+            <p>Only admins should see this!</p>
+        </div>
+    );
+}
---
+++ frontend/src/pages/SweetListPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SweetListPage() {
+    return (
+        <div className="container mt-5">
+            <h2>Our Sweets</h2>
+            <p>List of yummy sweets will go here.</p>
+        </div>
+    );
+}
